6a58ec6c5434976c58c0b34b45165e02a5bfdd4d
Evan Arafat <evanarafatbd@aol.com>
2020-04-09 06:03:22 -0400

    Definition commit

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -8,7 +8,7 @@ public class CollectionView {
 
 public static void main(String[] args) {
         /*
-         Map is created and inserted some data.Retrieve the Collection view of values present in map
+         Map is created and inserted some data. Retrieve the Collection view of values present in map
          */
         HashMap<Integer, String> map = new HashMap<Integer, String>();
         map.put(1, "NYC");
